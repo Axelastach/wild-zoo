269eaa88fb834cb4e48165d9a397006f738115c1
unknown <axelpicaut@orange.fr>
2023-04-06 15:35:43 +0200

    import logo nav bar

---
+++ wild-zoo/src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+        <div className='Navbar '>
+           <a href='/'> <img src={logo} className='logo' alt='log zoo' /></a>
+
+
+
+        </div>
+
+        
+        
+    )
+}
+
+export default Navbar
